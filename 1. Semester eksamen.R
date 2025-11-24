4aeea5ad612d95b5abbc754064e2d56198d46aa5
EmilMadsen20 <emma5@student.eadania.dk>
2025-11-24 10:29:29 +0100

    Mere flydende kode
    Mere flydende kode

--- 1. Semester eksamen.R
+++ 1. Semester eksamen.R
@@ -7,19 +7,18 @@ pacman::p_load("tidyverse", "magrittr", "nycflights13", "gapminder",
                "boot", "glmnet", "pls", "rvest", "DBI", "RSQLite",
                "lubridate", "rlist", "rjstat", "rjson", "Rcrawler", "usethis")
 
-# Indhentning af datafiler fra VFF
 
-# RDS filer
+# Indhentning af datafiler fra VFF ----------------------------------------
+
+  # RDS filer
 fcidk <- readRDS("data/fcidk.rds")
 vffkort01 <- readRDS("data/vffkort01.rds")
 
 View(fcidk)
 View(vffkort01)
 
-
-# SQL fil
+  # SQL fil
 con <- dbConnect(SQLite(), "data/fodbolddata.sqlite")
-
 dbListTables(con)
 
 db_vff <- dbReadTable(con, "db_vff")
@@ -29,8 +28,7 @@ view(db_vff)
 view(db_fcidk)
 
 
-# Superstats crawl
-
+# Superstats crawl --------------------------------------------------------
 superstats_program <- list()
 
 for (y in 2023:2025) {
@@ -43,7 +41,6 @@ for (y in 2023:2025) {
     html_table(header = FALSE, convert = FALSE)
   
   superstats_program[[as.character(y)]] <- alle_tabeller
-  
 }
 
 # Kør denne linje for at se data fra Superstats
@@ -52,7 +49,8 @@ superstats_program
 # Laver alt data til én dataframe
 superstats_dataframe <- bind_rows(superstats_program, .id = "runde")
 view(superstats_dataframe)
-#____________________Rasmus rettelse____________________________________________
+
+#Rensning af data fra Superstats
 superstats_dataframe <- superstats_dataframe[ , -c(7, 8)]
 
 superstats_dataframe <- superstats_dataframe[
@@ -61,11 +59,9 @@ superstats_dataframe <- superstats_dataframe[
 
 superstats_dataframe <- superstats_dataframe |>
   rename(Runde = runde,Ugedag = X1,Dato = X2,Hold = X3,Resultat = X4,Tilskuertal = X5)
-### ---------------------------------------------------------------------------
 
 
-# Danske helligdage fra Nager.Date
-
+# Helligdage fra Nager.Date -----------------------------------------------
 helligdage_list <- list()
 
 for (y in 2023:2025) {
@@ -86,39 +82,28 @@ for (y in 2023:2025) {
 # Saml alle år til ét dataframe
 helligdage_df <- bind_rows(helligdage_list)
 
-# Opdater datasættet
 helligdage_df <- helligdage_df |> 
   dplyr::select(date, localName) |> 
   rename(
     dato = date,
     helligdag = localName
-  )  
-str(helligdage_df)
-
-# Laver det fra character til dato
-helligdage_df <- helligdage_df %>%
-  mutate(dato = ymd(dato))
-
-# Fjerner alle banklukkedage fra datasættet
-helligdage_df <- helligdage_df %>%
-  filter(helligdag != "Banklukkedag")
+  )  |> 
+  dplyr::mutate(
+    dato = as.Date(dato)
+  ) |> 
+  dplyr::filter(helligdag != "Banklukkedag")
 
 view(helligdage_df)
 
 
-#### DMI data ------------------------------------------------------------------
-
-# Base URL og API-nøgle
+# DMI data ----------------------------------------------------------------
+  # Base URL og API-nøgle
 dmi_base_url <- "https://dmigw.govcloud.dk/v2/"
 dmi_info_url <- "metObs/collections/observation/items?"
 api_key <- Sys.getenv("MY_API_KEY")
 
-
-# Forbedret funktion med fejlhåndtering
-hent_dmi_data <- function(station_id, parameter_id, start_date, end_date, limit, season_label = NULL) {
-  
-
-  # Byg query
+# Funktion
+  hent_dmi_data <- function(station_id, parameter_id, start_date, end_date, limit, season_label = NULL) {
   query <- paste0(
     "stationId=", station_id,
     "&parameterId=", parameter_id,
@@ -159,97 +144,66 @@ hent_dmi_data <- function(station_id, parameter_id, start_date, end_date, limit,
   return(df_selected)
 }
 
+# År der skal hentes
+år <- 2023:2025
+karup <- "06060"
+vejr_list <- list()
 
-# Liste over sæsoner og deres datoer
-saesoner <- tibble::tibble(
-  season_label = c("19/20", "20/21", "21/22", "22/23", "23/24"),
-  start_date   = c("2019-07-31T00:00:00", "2020-07-31T00:00:00", "2021-07-31T00:00:00", "2022-07-17T00:00:00", "2023-07-28T00:00:00"),
-  end_date     = c("2020-05-15T23:59:00", "2021-05-15T23:59:00", "2022-05-15T23:59:00", "2023-05-29T23:59:00", "2024-05-25T23:59:00")
-)
-
-## Vind parameter
-vind_list <- list()
-
-for (i in 1:nrow(saesoner)) {
+for (y in år) {
+  start <- paste0(y, "-01-01T00:00:00")
+  slut  <- paste0(y, "-12-31T23:59:59")
   
-  season <- saesoner$season_label[i]
-  start  <- saesoner$start_date[i]
-  slut   <- saesoner$end_date[i]
+  cat("Henter Vind, Temp og Nedbør for år:", y, "\n")
   
-  cat("Henter sæson:", season, "\n")
-  
-  vind_list[[season]] <- hent_dmi_data(
-    station_id    = "06060",
+  # Vind
+  vind_y <- hent_dmi_data(
+    station_id    = karup,
     parameter_id  = "wind_speed_past1h",
     start_date    = start,
     end_date      = slut,
-    limit         = 50000,
-    season_label  = season
-  )
-}
-
-vind <- bind_rows(vind_list, .id = "sæson")
-
-## Temperatur parameter
-temp_list <- list()
-
-for (i in 1:nrow(saesoner)) {
+    limit         = 200000,
+    season_label  = as.character(y)
+  ) |> 
+    dplyr::mutate(type = "vind")
   
-  season <- saesoner$season_label[i]
-  start  <- saesoner$start_date[i]
-  slut   <- saesoner$end_date[i]
-  
-  cat("Henter TEMP for sæson:", season, "\n")
-  
-  temp_list[[season]] <- hent_dmi_data(
-    station_id    = "06060",
+  # Temperatur
+  temp_y <- hent_dmi_data(
+    station_id    = karup,
     parameter_id  = "temp_mean_past1h",
     start_date    = start,
     end_date      = slut,
-    limit         = 50000,
-    season_label  = season
-  )
-}
-
-temp <- dplyr::bind_rows(temp_list, .id = "sæson")
-
-## Nedbør parameter
-nedboer_list <- list()
-
-for (i in 1:nrow(saesoner)) {
+    limit         = 200000,
+    season_label  = as.character(y)
+  ) |> 
+    dplyr::mutate(type = "temp")
   
-  season <- saesoner$season_label[i]
-  start  <- saesoner$start_date[i]
-  slut   <- saesoner$end_date[i]
-  
-  cat("Henter NEDBØR for sæson:", season, "\n")
-  
-  nedboer_list[[season]] <- hent_dmi_data(
-    station_id    = "06060",
+  # Nedbør
+  nedboer_y <- hent_dmi_data(
+    station_id    = karup,
     parameter_id  = "precip_past1h",
     start_date    = start,
     end_date      = slut,
-    limit         = 50000,
-    season_label  = season
+    limit         = 200000,
+    season_label  = as.character(y)
+  ) |> 
+    dplyr::mutate(type = "nedbør")
+  
+  # Saml for året
+  vejr_list[[as.character(y)]] <- dplyr::bind_rows(
+    vind_y,
+    temp_y,
+    nedboer_y
   )
 }
 
-nedboer <- dplyr::bind_rows(nedboer_list, .id = "sæson")
-
-
-# Samler det hele
-vejr_all <- dplyr::bind_rows(
-  vind  |> dplyr::mutate(type = "vind"),
-  temp  |> dplyr::mutate(type = "temp"),
-  nedboer |> dplyr::mutate(type = "nedbør")
-)
-
-View(vejr_all)
+vejr_all <- dplyr::bind_rows(vejr_list, .id = "år")
 
 # Omdanner til wide format med pivot, altså så alle værdier får deres egen kolonner
-vejr_wide <- vejr_all %>%
-  dplyr::select(sæson, observationstidspunkt, type, værdi) %>%
+vejr_wide <- vejr_all |> 
+  dplyr::select(år, observationstidspunkt, type, værdi) |> 
   tidyr::pivot_wider(
     names_from = type,
     values_from = værdi
   )
+
+view(vejr_wide)
